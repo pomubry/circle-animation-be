5b0045f044b0095a63ea8a3d7ba1fff42d8cf042
pomubry <btaduran30@protonmail.com>
2021-01-02 15:43:43 +0800

    added back creds

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: 'https://circle-animation-fe.netlify.app',
-  // credentials: true,
-  // maxAge: 86400,
+  credentials: true,
 };
 
 app.use(express.json({ limit: '1mb' }));
